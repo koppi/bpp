663eefb8d9480efe881bcb7c7735125892bf69c1
Jakob Flierl <jakob.flierl@gmail.com>
2025-07-20 07:09:56 +0200

    tidy

--- scripts/povray-3.8.0.sh
+++ scripts/povray-3.8.0.sh
@@ -1,18 +1,17 @@
 #!/usr/bin/env bash
 
+export POV_DIR=povray-3.8.0
+export POV_CFG="--prefix=/opt/povray-3.8.0 --program-prefix=pov-3.8.0- --enable-watch-cursor"
+
 sudo apt -y install build-essential devscripts automake libboost-dev libboost-thread-dev zlib1g-dev libpng-dev libjpeg-dev libtiff5-dev libopenexr-dev libsdl1.2-dev
 
-export POV_DIR=povray-3.8.0
-
-git clone https://github.com/bullet-physics-playground/povray povray-3.8.0
+git clone https://github.com/bullet-physics-playground/povray $POV_DIR
 
 cd $POV_DIR
 
  cd unix/ ; ./prebuild.sh ; cd ../
 
 bash bootstrap
-
-export POV_CFG="--prefix=/opt/povray-3.8.0 --program-prefix=pov-3.8.0- --enable-watch-cursor"
 
 ./configure $POV_CFG COMPILED_BY="Jakob Flierl <jakob.flierl@gmail.com>"
 
