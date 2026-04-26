3da90c5a125f6fb8d1fa593342c61547b46b84f5
Jakob Flierl <jakob.flierl@gmail.com>
2026-04-26 17:55:14 +0200

    Update 07-gear.lua demo
    - Rotate gears around z-axis instead of y-axis
    - Add hinge without motor for gearB
    - Move gears to center (x=-1 and x=1)
    - Adjust camera to look at origin

--- demo/constraint/07-gear.lua
+++ demo/constraint/07-gear.lua
@@ -7,44 +7,50 @@ plane = Plane(0,1,0,0,10)
 plane.col = "#222222"
 v:add(plane)
 
-theta = math.pi / 4
+theta = math.pi / 8
 
 l1 = 2 - math.tan(theta)
 l2 = 1 / math.cos(theta)
 ratio = l2 / l1
 
-gearA = Cylinder(0.5, 0.25, 1)
-gearA.pos = btVector3(-8, 1, -8)
-gearA.col = "#444444"
+gearA = Cylinder(1, 0.25, 1)
+gearA.pos = btVector3(-1, 1, 0)
+gearA.col = "#ff0000"
 v:add(gearA)
 
-gearB = Cylinder(1.2, 0.26, 1)
-gearB.pos = btVector3(-10, 2, -8)
+gearB = Cylinder(0.5, 0.26, 1)
+gearB.pos = btVector3(0.5, 1, 0)
 q = btQuaternion()
 q:setEulerZYX(0, 0, -theta)
 gearB.rot = q
-gearB.col = "#ff6600"
+gearB.col = "#006600"
 v:add(gearB)
 
 gearA.body:setLinearFactor(btVector3(0, 0, 0))
-gearA.body:setAngularFactor(btVector3(0, 1, 0))
+gearA.body:setAngularFactor(btVector3(0, 0, 1))
 
 hinge = btHingeConstraint(
   gearA.body,
   btVector3(0, 0, 0),
-  btVector3(0, 1, 0))
+  btVector3(0, 0, 1))
 hinge:enableAngularMotor(true, 2, 10)
 v:addConstraint(hinge)
 
 con = btGearConstraint(
   gearA.body, gearB.body,
-  btVector3(0, 1, 0), btVector3(0, 1, 0),
+  btVector3(0, 0, 1), btVector3(0, 0, 1),
   ratio)
 
 v:addConstraint(con)
 
+hingeB = btHingeConstraint(
+  gearB.body,
+  btVector3(0, 0, 0),
+  btVector3(0, 0, 1))
+v:addConstraint(hingeB)
+
 v.cam:setUpVector(btVector3(0,1,0), true)
-v.cam.pos = btVector3(-8, 0, 0)
-v.cam.look = btVector3(-8, 0, -8)
+v.cam.pos = btVector3(0, 2, 3)
+v.cam.look = btVector3(0, 1, 0)
 
 -- EOF
