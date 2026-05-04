c6d814c220aca54bb3600a2a64b124a5bd35d596
Jakob Flierl <jakob.flierl@gmail.com>
2026-05-04 20:09:28 +0200

    Add -CC option

--- src/viewer.cpp
+++ src/viewer.cpp
@@ -2255,7 +2255,7 @@ void Viewer::onQuickRender(QString povargs) {
   QString systemPovExe = QStandardPaths::findExecutable(defaultPovrayExe);
   if (systemPovExe.isEmpty()) systemPovExe = "POV-Ray not found!";
 
-  QString defaultPreview = QString("%1 -c +d -A +p +Q11 +GA").arg(defaultIncludes);
+  QString defaultPreview = QString("%1 -c +d -A +p +Q11 +GA -CC").arg(defaultIncludes);
 
   QString povray = _settings->value("povray/executable", systemPovExe).toString();
   QString opts =   _settings->value("povray/preview", defaultPreview).toString();
